a17392663aad77e1725e44943623832ec1632f63
Bayram Turgut <137455737+bayramtturgutt@users.noreply.github.com>
2024-08-02 15:13:23 +0000

    run

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/users")
+    public String getUsers() {
+        return "List of users";
+    }
+}
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -10,7 +10,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
 
     // Getters and Setters
 }
